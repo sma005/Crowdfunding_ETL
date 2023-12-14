a580afe37d103abf9a795c772a9045f1676b7eec
madhaught <144740749+madhaught@users.noreply.github.com>
2023-12-13 19:36:55 -0500

    changed schema sql file

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -2,26 +2,6 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "campaign" (
-    "cf_id" int   NOT NULL,
-    "contact_id" int   NOT NULL,
-    "company_name" varchar(255)   NOT NULL,
-    "description" varchar(255)   NOT NULL,
-    "goal" float   NOT NULL,
-    "pledged" float   NOT NULL,
-    "outcome" varchar(15)   NOT NULL,
-    "backers_count" int   NOT NULL,
-    "country" varchar(50)   NOT NULL,
-    "currency" char(3)   NOT NULL,
-    "launched_date" date   NOT NULL,
-    "end_date" date   NOT NULL,
-    "category_id" varchar(10)   NOT NULL,
-    "subcategory_id" varchar(10)   NOT NULL,
-    CONSTRAINT "pk_campaign" PRIMARY KEY (
-        "cf_id"
-     )
-);
-
 CREATE TABLE "category" (
     "category_id" varchar(10)   NOT NULL,
     "category" varchar(50)   NOT NULL,
@@ -45,6 +25,26 @@ CREATE TABLE "subcategory" (
     "subcategory" varchar(50)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(255)   NOT NULL,
+    "description" varchar(255)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(15)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(50)   NOT NULL,
+    "currency" char(3)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
      )
 );
 
